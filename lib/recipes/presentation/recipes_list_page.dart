c2ed7609dd64022777a2e9ae85a8b5d50eadf68f
Maatteogekko <matteogent9991@gmail.com>
2021-10-18 22:10:56 +0200

    fixed scroll

--- lib/recipes/presentation/recipes_list_page.dart
+++ lib/recipes/presentation/recipes_list_page.dart
@@ -41,64 +41,67 @@ class _RecipesListPageState extends State<RecipesListPage> {
           centerTitle: true,
         ),
         body: Scrollbar(
-          child: CenteredLayout(
-            maxWidth: maxWidth,
-            child: Consumer(
-              builder: (context, ref, child) {
-                final notifier = ref.read(recipesNotifierProvider.notifier);
-                final state = ref.watch(recipesNotifierProvider);
+          controller: _controller,
+          child: ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+            child: CenteredLayout(
+              maxWidth: maxWidth,
+              child: Consumer(
+                builder: (context, ref, child) {
+                  final notifier = ref.read(recipesNotifierProvider.notifier);
+                  final state = ref.watch(recipesNotifierProvider);
 
-                return state.when(
-                  initial: (_) {
-                    SchedulerBinding.instance?.addPostFrameCallback((_) => notifier.loadRecipes());
-                    return const SizedBox.shrink();
-                  },
-                  loading: (_) => const Center(
-                    child: CircularProgressIndicator.adaptive(),
-                  ),
-                  failure: (_, message) => Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        message,
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: notifier.loadRecipes,
-                        child: const Text("Refresh"),
-                      ),
-                    ],
-                  ),
-                  data: (recipes) => RefreshIndicator(
-                    onRefresh: notifier.loadRecipes,
-                    child: width < maxWidth
-                        ? ListView.builder(
-                            controller: _controller,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: state.recipes.length,
-                            padding: const EdgeInsets.all(16),
-                            itemBuilder: (context, index) => Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 16),
-                              child: RecipeCard(
-                                recipe: state.recipes[index],
+                  return state.when(
+                    initial: (_) {
+                      SchedulerBinding.instance
+                          ?.addPostFrameCallback((_) => notifier.loadRecipes());
+                      return const SizedBox.shrink();
+                    },
+                    loading: (_) => const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    ),
+                    failure: (_, message) => Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          message,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: notifier.loadRecipes,
+                          child: const Text("Refresh"),
+                        ),
+                      ],
+                    ),
+                    data: (recipes) => RefreshIndicator(
+                      onRefresh: notifier.loadRecipes,
+                      child: width < maxWidth
+                          ? ListView.builder(
+                              controller: _controller,
+                              itemCount: state.recipes.length,
+                              padding: const EdgeInsets.all(16),
+                              itemBuilder: (context, index) => Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 16),
+                                child: RecipeCard(
+                                  recipe: state.recipes[index],
+                                ),
                               ),
+                            )
+                          // FIXME this throws a non-fatal exception
+                          : StaggeredGridView.countBuilder(
+                              controller: _controller,
+                              padding: const EdgeInsets.all(16),
+                              staggeredTileBuilder: (index) => const StaggeredTile.fit(1),
+                              crossAxisCount: 2,
+                              itemCount: state.recipes.length,
+                              itemBuilder: (context, index) =>
+                                  RecipeCard(recipe: state.recipes[index]),
                             ),
-                          )
-                        // FIXME this throws a non-fatal exception
-                        : StaggeredGridView.countBuilder(
-                            controller: _controller,
-                            physics: const NeverScrollableScrollPhysics(),
-                            padding: const EdgeInsets.all(16),
-                            staggeredTileBuilder: (index) => const StaggeredTile.fit(1),
-                            crossAxisCount: 2,
-                            itemCount: state.recipes.length,
-                            itemBuilder: (context, index) =>
-                                RecipeCard(recipe: state.recipes[index]),
-                          ),
-                  ),
-                );
-              },
+                    ),
+                  );
+                },
+              ),
             ),
           ),
         ),
